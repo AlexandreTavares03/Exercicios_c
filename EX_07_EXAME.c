f8eb7a54058f5b6d4d4e49dcfbd8cb278be00c75
alexandre <alexandre_tav03@outlook.com>
2026-07-03 09:50:18 -0300

    Comentários adicionados para cada sub rotina

--- EX_07_EXAME.c
+++ EX_07_EXAME.c
@@ -10,20 +10,40 @@ e cabelos pretos.
 idade entre 18 e 35 anos (inclusive) e que tenham olhos azuis e cabelos louros. /*/
 
 #include <stdio.h>
+/*/ Nessa sub rotina o programa lê os dados e armazena nos vetores. O i=0; é usado como uma contador para o for, e
+não possui o "retun" pois o programa só armazena e não pede pra devolver nada./*/
 
 void LerDados(char sexo[], char cor_olhos[], char cor_cabelo[], int idade[], int N){
 	int i =0;
 	
-	printf("=======DADOS DA GALERA========= \n");
+	printf("\n=======DADOS DA GALERA========= \n");
 	
 	for(i=0 ; i<N; i++){
 	printf("\n DIGITE IDADE:"); scanf("%d", &idade[i]);
-	printf("\n DIGITE SEXO:"); scanf(" %c", &sexo[i]);
+	printf("\n DIGITE SEXO: (M)- Masculino, (F)- Feminino:"); scanf(" %c", &sexo[i]);
 	printf("\n DIGITE COR DO CABELO (L)- Loiro, (P)- Preto, (C)- Castanho:"); scanf(" %c", &cor_cabelo[i]);
 	printf("\n DIGITE COR DOS OLHOS (A)- Azul, (C)- Castanho:"); scanf(" %c", &cor_olhos[i]);
 	}	
 }
+/*/ Nessa sub rotina o programa testa algumas condições impostas, soma e devolve através do return quais individuos
+que se enquadram nas caracteristicas e soma mais 1 no contador "quan_fem"./*/
 
+int QuantFeminino(char sexo[], char cor_olhos[], char cor_cabelo[], int idade[], int N){
+	int quant_fem=0;
+	int i=0;
+	
+	for (i=0; i<N; i++){
+		if(sexo[i]== 'F' && cor_olhos[i]== 'A' && cor_cabelo[i]== 'L' && idade[i]>=18 && idade[i]<=35){
+		
+			quant_fem= quant_fem +1;
+		}
+	}
+	return quant_fem;
+}
+/*/ Nessa sub rotina o programa adicionei por conta própria uma média de idade, aqui adicionei 
+uma variavel "media=0" para que a mesma pudesse somar as idades de todos habitantes que forem digitadas
+ e dividir pela quantidade de habitantes./*/
+ 
 float MediaIdade(int idade[], int N){
 	int i=0;
 	float media=0;
@@ -34,6 +54,7 @@ float MediaIdade(int idade[], int N){
 	
 	return media;
 }
+/*/ Nessa sub rotina o programa percorre o vetor em busca da maior idade digitada./*/
 
 int MaiorIdade( int idade[], int N){
 	int i=0;
@@ -46,6 +67,11 @@ int MaiorIdade( int idade[], int N){
 	}
 	return maior;
 }
+
+/*/ Nessa sub rotina ela faz a media de idade de pessoas que possuem duas caracteristiscas especificas iguais,
+aqui foi implementadas condições p/ percorrer o vetor, uma variavel "soma_med" cridada para somar as idades dos habitantes 
+que se enquadram na condição proposta e uma variavel"cont" para somar 1 a cada vez que a condição é alcançada e posteriormente
+dividir a soma das idades pelo contador (soma_med/cont), isso retornado a média que foi solicitada./*/
 
 float MedIdadeOlhoCabelo(int N, char cor_olhos[], char cor_cabelo[],int idade[]){
 	float soma_med=0;
@@ -68,23 +94,32 @@ float MedIdadeOlhoCabelo(int N, char cor_olhos[], char cor_cabelo[],int idade[])
 }
 
 int main(){
+	/*/ No programa principal  adicionei para que o usuário defina quantos habitantes, por isso sempre inicio a variavel N
+	para que a primeira mensagem que apareça seja pro usuário definir a qtd e a partir disso são definidos
+	a qtd de vetores que serão percorridos e quantas vezes o for vai repetir as msg pro usuário/*/
 	int N;
-	
 	printf("INFORME O VALOR DE HABITANTES:"); scanf("%d", &N);
 	
+	/*/ Aqui são as variáveis usadas no retun e no programa principal depois que o vetor "N" foi iniciado de fato/*/
 	char sexo[N], cor_olhos[N], cor_cabelo[N];
 	int idade[N];
 	float media, soma_med;
-	int maior;
+	int maior, quant_fem;
 	
+	/*/ Aqui no programa principal é onde acontece a chamada das sub rotinas que foram feitas, 
+	e é aqui que elas são executadas no programa principal/*/
 	LerDados(sexo, cor_olhos, cor_cabelo, idade, N);
 	media= MediaIdade(idade, N);
 	maior= MaiorIdade(idade, N);
 	soma_med= MedIdadeOlhoCabelo(N, cor_olhos, cor_cabelo, idade);
+	quant_fem= QuantFeminino(sexo, cor_olhos, cor_cabelo, idade, N);
 	
-	printf("MEDIA DE IDADE DOS HABITANTES E: %.2f", media);
-	printf("MAIOR IDADE É: %d", maior);
-	printf("MÉDIA DE IDADE DE HABITANTES QUE TEM OLHOS CASTANHOS E CABELOS PRETOS E: %.2f", soma_med);
+	/*/ Aqui exibe o resultado das funções que foram feitas para que o usuário veja/*/
+	printf("\n ===========================================");
+	printf("\n MEDIA DE IDADE DOS HABITANTES E: %.2f \n", media);
+	printf("\n MAIOR IDADE É: %d \n", maior);
+	printf("\n MÉDIA DE IDADE DE HABITANTES QUE TEM OLHOS CASTANHOS E CABELOS PRETOS E: %.2f \n", soma_med);
+	printf("\n A QUANTIDADE DE HABITANTES DO SEXO FEMININO E QUE POSSUEM AS CARACTERISTICAS INOFORMADAS SAO: %d \n", quant_fem);
 	
 	return 0;
 }
